2ee72b4e7f6178e32aece6ecd6ad25cc3ae807f6
nieoh <steph@nieoh.com>
2018-08-13 23:41:21 -0700

    added solution

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,7 +2,15 @@ from test_framework import generic_test
 
 
 def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
-    # TODO - you fill in here.
+    
+    if tree is None:
+        return True
+    if tree.data < low_range or tree.data > high_range:
+        return False
+    if not is_binary_tree_bst(tree.left, low_range=low_range, high_range=tree.data):
+        return False
+    if not is_binary_tree_bst(tree.right, low_range=tree.data, high_range=high_range):
+        return False
     return True
 
 
