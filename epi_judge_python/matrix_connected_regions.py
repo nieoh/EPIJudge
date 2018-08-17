fa808f72b552a79aaec60cf68135a2b7cf94bb20
nieoh <steph@nieoh.com>
2018-08-16 19:03:58 -0700

    DFS O(m*n) since each point is visited at most once most those guys are visited. no extra space

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def flip_color(x, y, image):
-    # TODO - you fill in here.
-    return
+    color = image[x][y]
+    def flip(curr):
+        a, b = curr
+        if not(0 <= a < len(image) and 0 <= b < len(image[a])) or image[a][b] != color:
+            return
+        image[a][b] = color^1
+        candidates = [(a-1, b), (a+1, b), (a, b-1), (a, b+1)]
+        for cand in candidates:
+            flip(cand)
+        return
+
+    flip((x, y))
+    return 
 
 
 def flip_color_wrapper(x, y, image):
