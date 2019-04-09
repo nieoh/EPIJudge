f5cc8a2cff65b795a76443af1a50a70c62b6cc19
nieoh <steph@nieoh.com>
2019-04-09 00:57:46 -0700

    O(n+m) time O(n+m) space

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -1,10 +1,52 @@
 from test_framework import generic_test
-
+from list_node import ListNode
 
 def add_two_numbers(L1, L2):
-    # TODO - you fill in here.
-    return None
+    L = ListNode()
+    dummy_head = L
+    carry = 0
 
+    while L1 and L2:
+        summ = L1.data + L2.data + carry
+        
+        if summ >= 10:
+            L.next = ListNode(summ%10)
+            carry = 1
+        else:
+            L.next=ListNode(summ)
+            carry = 0
+        L = L.next
+        L1, L2 = L1.next, L2.next
+
+    while L1:
+        if carry == 0:
+            L.next = L1
+            break
+        summ = L1.data + carry
+        if summ >= 10:
+            L.next = ListNode(summ%10)
+            carry = 1
+        else:
+            L.next = ListNode(summ)
+            carry = 0
+        L, L1 = L.next, L1.next
+
+    while L2:
+        if carry == 0:
+            L.next = L2
+            break
+        summ = L2.data + carry
+        if summ >= 10:
+            L.next = ListNode(summ%10)
+            carry = 1
+        else:
+            L.next = ListNode(summ)
+            carry = 0
+        L, L2 = L.next, L2.next
+
+    if carry != 0:
+        L.next = ListNode(carry)
+    return dummy_head.next
 
 if __name__ == '__main__':
     exit(
