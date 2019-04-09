fb18318611a45462fa656ae21ae680f062cbff56
nieoh <steph@nieoh.com>
2019-04-08 18:29:04 -0700

    phone number mnemonic. O(4^n) space. O(4^n) time

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -3,7 +3,19 @@ from test_framework import generic_test, test_utils
 
 def phone_mnemonic(phone_number):
     # TODO - you fill in here.
-    return []
+    MAPPING = ['0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+    if not phone_number:
+        return ['']
+
+    ret = []
+    stuff = phone_mnemonic(phone_number[:-1])
+    chars = MAPPING[int(phone_number[-1])]
+    for shit in stuff:
+        for char in list(chars):
+            ret.append(shit + char)
+    
+            
+    return ret
 
 
 if __name__ == '__main__':
