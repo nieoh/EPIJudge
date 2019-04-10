f2a26bc271fc30aa1354b251c3c75037fcc37b7b
nieoh <steph@nieoh.com>
2019-04-09 23:01:02 -0700

    remember u can store tuples in stacks and stuff. O(n) time O(n) space

--- epi_judge_python/sunset_view.py
+++ epi_judge_python/sunset_view.py
@@ -3,7 +3,13 @@ from test_framework import generic_test
 
 def examine_buildings_with_sunset(sequence):
     # TODO - you fill in here.
-    return []
+    if not sequence:
+        return []
+    ret = [(sequence[-1], len(sequence)-1)]
+    for i, h in enumerate(reversed(sequence[:-1])):
+        if h > ret[-1][0]:
+            ret.append((h, len(sequence)-i-2))
+    return [r[1] for r in ret]
 
 
 def examine_buildings_with_sunset_wrapper(sequence):
