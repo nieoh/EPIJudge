80dcb658f036ba6c22c6b891f684b556f7faa206
nieoh <steph@nieoh.com>
2019-04-11 19:04:32 -0700

    O(n) time O(1) space

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,7 +3,15 @@ from test_framework import generic_test
 
 def is_symmetric(tree):
     # TODO - you fill in here.
-    return True
+    def func(node1, node2):
+        if not node1 and not node2:
+            return True
+        elif not node1 or not node2:
+            return False
+        return (node1.data == node2.data) and (func(node1.left, node2.right)) and (func(node1.right, node2.left))
+    if not tree:
+        return True
+    return func(tree.left, tree.right)
 
 
 if __name__ == '__main__':
