12681e90ce79fd92a619c143f55a2af6addef01b
nieoh <steph@nieoh.com>
2019-05-16 18:25:22 -0700

    O(log(n)) time O(1) space

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -3,7 +3,20 @@ from test_framework import generic_test
 
 def search_first_of_k(A, k):
     # TODO - you fill in here.
-    return 0
+    L, U = 0, len(A)-1
+
+    while L <= U:
+        M = L + (U-L)//2
+        if A[M] < k:
+            L = M+1
+        elif A[M] > k:
+            U = M-1
+        elif M==0 or A[M-1]!=k:
+            return M
+        else:
+            U = M-1
+
+    return -1
 
 
 if __name__ == '__main__':
