691353505afef34b11b40384dd8c25a11c11e8ca
nieoh <steph@nieoh.com>
2019-05-20 14:44:19 -0700

    O(log(k)) time O(1) space

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -3,6 +3,24 @@ from test_framework import generic_test
 
 def square_root(k):
     # TODO - you fill in here.
+    if k == 0:
+        return 0
+    if k <= 3:
+        return 1
+    L, U = 1, k//2
+    while L<=U:
+        M = L + (U-L)//2
+        if M**2 == k:
+            return M
+        elif M**2 > k:
+            U = M-1
+        elif (M+1)**2 > k:
+            return M
+        else:
+            L = M+1
+
+
+
     return 0
 
 
