095b5549a0aadf109552738b70fc9319d75c14c7
nieoh <steph@nieoh.com>
2019-05-21 19:53:03 -0700

    O(nlogn) time O(n) space

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,10 +1,18 @@
 from test_framework import generic_test
-
+from heapq import heappushpop, heappush, heappop
 
 def online_median(sequence):
     # TODO - you fill in here.
-    return []
+    min_heap=[]
+    max_heap=[]
+    ret = []
+    for x in sequence:
+        heappush(min_heap, -heappushpop(max_heap, -x))
 
+        if len(min_heap) > len(max_heap):
+            heappush(max_heap, -heappop(min_heap))
+        ret.append(0.5*(min_heap[0]-max_heap[0]) if len(min_heap)==len(max_heap) else -max_heap[0]) 
+    return ret
 
 def online_median_wrapper(sequence):
     return online_median(iter(sequence))
