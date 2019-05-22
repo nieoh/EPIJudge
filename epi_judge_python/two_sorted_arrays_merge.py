97497d3a8e4cb1eb2db6cc7e0113cb928d98a8b3
nieoh <steph@nieoh.com>
2019-05-21 19:28:34 -0700

    O(n+m) time O(1) space

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -3,8 +3,19 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A, m, B, n):
     # TODO - you fill in here.
-    return
+    i, j = m-1, n-1
+    
+    while i>=0 or j>=0:
+        if (j==-1) or (i >= 0 and A[i] >= B[j]):
+            A[i+j+1] = A[i]
+            i -= 1
 
+        else:
+            A[i+j+1] = B[j]
+            j -= 1
+
+
+    
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
     merge_two_sorted_arrays(A, m, B, n)
