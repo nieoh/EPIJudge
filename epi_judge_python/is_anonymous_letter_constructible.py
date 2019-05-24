01e8a66a22c770463dcf29256e3d37c6059f4198
nieoh <steph@nieoh.com>
2019-05-23 18:07:16 -0700

    O(m+n) time O(L) space

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -3,8 +3,24 @@ from test_framework import generic_test
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
     # TODO - you fill in here.
-    return True
+    if len(letter_text) > len(magazine_text):
+        return False
+    dic = {}
+    for ch in letter_text:
+        if ch in dic:
+            dic[ch] += 1
+        else:
+            dic[ch] = 1
 
+    for ch in magazine_text:
+        if ch in dic:
+            dic[ch] -= 1
+            if dic[ch] == 0:
+                del dic[ch]
+                if not dic:
+                    return True
+
+    return not dic
 
 if __name__ == '__main__':
     exit(
