73602faf6e2bd55f774513ce52c30d31b05b3f09
nieoh <steph@nieoh.com>
2019-05-29 18:25:10 -0700

    O(h) time and space where h is height

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -5,11 +5,27 @@ from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+def construct_ancestor(tree, node):
+    ret = [tree]
+    if not tree:
+        return []
+    if tree.data == node.data:
+        return ret
+    elif tree.data < node.data:
+        return ret+construct_ancestor(tree.right, node)
+    else:
+        return ret+construct_ancestor(tree.left, node)
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
 def find_LCA(tree, s, b):
     # TODO - you fill in here.
-    return None
+    ancestor1 = construct_ancestor(tree, s)
+    ancestor2 = construct_ancestor(tree, b)
+    i = 1
+    while i < min(len(ancestor1), len(ancestor2)) and ancestor1[i].data == ancestor2[i].data:
+        i += 1
+     
+    return ancestor1[i-1]
 
 
 @enable_executor_hook
