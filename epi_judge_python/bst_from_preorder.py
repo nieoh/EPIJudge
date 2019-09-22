f58765328d6c3e3fbca99e8f49a184ec1c1ac8d2
nieoh <steph@nieoh.com>
2019-09-22 14:05:34 -0700

    need to redo bst to dll bc i didnt do it in place. bst from preorder is O(n2), redo by adding upper and lower bounds instead. to ddl takes O(n) time and O(n) space bc i create new data and pointers

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
-
+from binary_tree_node import BinaryTreeNode 
 
 def rebuild_bst_from_preorder(preorder_sequence):
     # TODO - you fill in here.
-    return None
+    if not preorder_sequence:
+        return None
+    node = preorder_sequence[0]
+    i = 1
+    while i < len(preorder_sequence) and preorder_sequence[i]<node:
+        i += 1
+    node = BinaryTreeNode(data=node,
+            left=rebuild_bst_from_preorder(preorder_sequence[1:i]),
+            right=rebuild_bst_from_preorder(preorder_sequence[i:]))
+    return node
 
 
 if __name__ == '__main__':
--- epi_judge_python/bst_to_sorted_list.py
+++ epi_judge_python/bst_to_sorted_list.py
@@ -3,29 +3,43 @@ import functools
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
-
+from doubly_list_node import DoublyListNode
 
 def bst_to_doubly_linked_list(tree):
-    # TODO - you fill in here.
-    return None
+    def helper(tree):
+        if not tree:
+            return None, None
+        l = DoublyListNode(tree.data)
+        first = last = l
+        F, l.prev = helper(tree.left)
+        if l.prev:
+            l.prev.next = l
+        if F:
+            first = F
+        l.next , L = helper(tree.right)
+        if l.next:
+            l.next.prev = l
+        if L:
+            last = L
 
+        return first, last
+
+    head, _ = helper(tree)
+    return head
 
 @enable_executor_hook
 def bst_to_doubly_linked_list_wrapper(executor, tree):
     l = executor.run(functools.partial(bst_to_doubly_linked_list, tree))
-
-    if l is not None and l.left is not None:
+    if l is not None and l.prev is not None:
         raise TestFailure(
             'Function must return the head of the list. Left link must be None'
         )
-
     v = []
     while l:
         v.append(l.data)
-        if l.right and l.right.left is not l:
+        if l.next and l.next.prev is not l:
             raise TestFailure('List is ill-formed')
-        l = l.right
-
+        l = l.next
     return v
 
 
