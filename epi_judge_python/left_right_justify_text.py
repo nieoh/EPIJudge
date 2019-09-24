67fa1747af0d0f2a596aea6bfd1f92891fe7cdc3
nieoh <steph@nieoh.com>
2019-09-23 19:55:59 -0700

    O(total_num_chars)

--- epi_judge_python/left_right_justify_text.py
+++ epi_judge_python/left_right_justify_text.py
@@ -2,8 +2,44 @@ from test_framework import generic_test
 
 
 def justify_text(words, L):
-    # TODO - you fill in here.
-    return []
+    ret  = []
+    i=0
+    while i < len(words):
+        line = []
+        counter = 0
+        while i < len(words) and counter <= L:
+            tmp = words[i]
+            if counter+len(tmp) > L:
+                break
+            line.append(tmp)
+            counter += 1 + len(tmp)
+            i += 1
+        counter -= len(line)
+        # find space multiplier
+        if i == len(words):
+            tmp = ''
+            for word in line:
+                tmp += word + ' '
+            tmp += ' '*(L-counter)
+        else:
+            if len(line) != 1:
+                rem = (L-counter)%(len(line)-1)
+                mult = (L-counter)//(len(line)-1)
+            else:
+                mult = L - counter
+                rem = 0
+            
+            tmp = ''
+            for word in line:
+                tmp += word
+                extra = 0
+                if rem > 0:
+                    extra = 1
+                    rem = max(0, rem-1)
+                tmp += ' '*(mult+extra)
+        ret.append(tmp[:L])
+    
+    return ret
 
 
 if __name__ == '__main__':
